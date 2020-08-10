9947a11ef2c96345acb15b5dfa84dde5d603b3fc
ericsungjinkim <sungjin.kim@polytechnique.edu>
2020-08-10 21:54:49 +0200

    수정

--- Lab3_Dummy_Q_learning.py
+++ Lab3_Dummy_Q_learning.py
@@ -11,20 +11,20 @@ def rargmax(vector):
 
 register(
     id='FrozenLake-v3',
-    entry_point='gum.envs.toy_text:FrozenLakeEnv',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
     kwargs={'map_name':'4x4',
             'is_slippery':False}
 )
 env = gym.make('FrozenLake-v3')
 
 # Intialize table with all zeros
-Q = np.zeors([env.observation_space.n, env.action_space.n])
+Q = np.zeros([env.observation_space.n, env.action_space.n])
 # Set learning parameters
 num_episodes = 2000
 
 # Create lists to contaion total rewards and steps per episode
 rList = []
-for i range(num_episodes):
+for i in range(num_episodes):
     # Reset environment and get first new ovservation
     state = env. reset()
     rAll = 0
